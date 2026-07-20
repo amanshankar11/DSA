bc4df6cfe62dd71bcb66f97b875e67d63ab8493f
AMAN SHANKAR <153946946+amanshankar11@users.noreply.github.com>
2026-07-20 19:04:38 +0530

    Time: 0 ms (100%), Space: 48.1 MB (51.53%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,53 +1,40 @@
 class Solution {
 
-    public int firstOccurence(int target,int low,int high,int[] arr){
-        if(low>high){
-            return -1;
-        }
+    public int leftOccurence(int target,int low,int high,int[] nums){
+        if(low>high) return -1;
 
         int mid=low+(high-low)/2;
 
-        if(arr[mid]==target){
-            int left=firstOccurence(target,low,mid-1,arr);
-            return left==-1?mid:left;
-        }
-        else if(arr[mid]<target){
-           return firstOccurence(target,mid+1,high,arr);
-        }
-        else{
-            return firstOccurence(target,low,mid-1,arr);
-        }
-    }
-    public int lastOccurrence(int target,int low,int high,int[] arr) {
-    if (low>high){
-        return -1;
-    }
-    int mid=low+(high-low)/2;
 
-    if (arr[mid]==target) {
-        int right=lastOccurrence(target,mid+1,high,arr);
-        return right==-1?mid:right;
+        if(nums[mid]==target){
+            int left=leftOccurence(target,low,mid-1,nums);
+            if(left==-1) return mid;
+            return left;
+        }
+        else if(nums[mid]>target){
+            return leftOccurence(target,low,mid-1,nums);
+        }
+            return leftOccurence(target,mid+1,high,nums);
     }
+    public int rightOccurence(int target,int low,int high,int[] nums){
+        if(low>high) return -1;
 
-    if (arr[mid]<target){
-        return lastOccurrence(target,mid+1,high,arr);
+        int mid=low+(high-low)/2;
+
+        if(nums[mid]==target){
+            int right=rightOccurence(target,mid+1,high,nums);
+            if(right==-1) return mid;
+            return right;
+        }
+        else if(nums[mid]>target){
+            return rightOccurence(target,low,mid-1,nums);
+        }
+            return rightOccurence(target,mid+1,high,nums);
     }
-    else{
-        return lastOccurrence(target,low,mid-1,arr);
-    }
-}
     public int[] searchRange(int[] nums, int target) {
-        int[] res=new int[2];
-        // if(nums.length==1 && target==nums[0]){
-        //     Arrays.fill(res,0);
-        //     return res;
-        // }
-        // res[0]=-1;
-        // res[1]=-1;
-        int low=0;
-        int high=nums.length-1;
-        res[0]=firstOccurence(target,0,nums.length-1,nums);
-        res[1]=lastOccurrence(target,0,nums.length-1,nums);
-        return res;
+        int[] ans=new int[2];
+        ans[0]=leftOccurence(target,0,nums.length-1,nums);
+        ans[1]=rightOccurence(target,0,nums.length-1,nums);
+        return ans;
     }
 }
