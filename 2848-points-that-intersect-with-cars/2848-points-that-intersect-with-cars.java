d7bb0d5bd943216af8a86a37f467586d68fb854c
AMAN SHANKAR <153946946+amanshankar11@users.noreply.github.com>
2026-08-03 18:24:58 +0530

    Time: 1 ms (100%), Space: 46.4 MB (51.42%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
@@ -1,16 +1,16 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
-        boolean[] arr=new boolean[101];
+        int[] arr=new int[102];
         for(int i=0;i<nums.size();i++){
             int start=nums.get(i).get(0);
             int end=nums.get(i).get(1);
-            for(int j=start;j<=end;j++){
-                arr[j]=true;
-            }
+            arr[start]+=1;
+            arr[end+1]-=1;
         }
         int ans=0;
-        for(boolean i:arr){
-            if(i) ans++;
+        for(int i=1;i<arr.length;i++){
+            arr[i]+=arr[i-1];
+            if(arr[i]!=0) ans++;
         }
         return ans;
     }
