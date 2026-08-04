55445e27349126899091fe8d7a11f3573ac1d44c
AMAN SHANKAR <153946946+amanshankar11@users.noreply.github.com>
2026-08-04 09:40:36 +0530

    Time: 98 ms (17.36%), Space: 72.8 MB (25.62%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,18 +1,20 @@
 class Solution {
+    private boolean helper(Boolean[][] dp,int[] nums,int i,int sum,int target){
+        if(i>=nums.length || sum>target)return false;
+        if(target==sum)return true;
+        if(dp[i][sum]!=null)return dp[i][sum];
+        boolean pick=helper(dp,nums,i+1,sum+nums[i],target);
+        boolean notPick=helper(dp,nums,i+1,sum,target);
+        dp[i][sum]=pick||notPick;
+        return dp[i][sum];
+    }
     public boolean canPartition(int[] nums) {
         int total=0;
         for(int x:nums)total+=x;
         if(total%2!=0) return false;
         int target=total/2;
-        for(int i=0;i<(1<<nums.length);i++){
-            int sum=0;
-            for(int j=0;j<nums.length;j++){
-                if(((i>>j)&1)==1){
-                    sum+=nums[j];
-                }
-            }
-            if(sum==target)return true;
-        }
-        return false;
+        Boolean[][] dp=new Boolean[nums.length][target+1];
+        Boolean ans=helper(dp,nums,0,0,target);
+        return ans;
     }
 }
