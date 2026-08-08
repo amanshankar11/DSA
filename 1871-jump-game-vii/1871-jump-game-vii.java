9622f70278f73b4cdca4a8d31290556dce51ccd5
AMAN SHANKAR <153946946+amanshankar11@users.noreply.github.com>
2026-08-08 20:23:19 +0530

    Time: 11 ms (42.94%), Space: 48 MB (43.89%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        boolean[] dp = new boolean[n];
+        dp[0] = true;
+        int reachable = 0;
+        for (int i = 1; i < n; i++) {
+            int add = i - minJump;
+            if (add >= 0 && dp[add]) {
+                reachable++;
+            }
+            int remove = i - maxJump - 1;
+            if (remove >= 0 && dp[remove]) {
+                reachable--;
+            }
+            if (s.charAt(i) == '0' && reachable > 0) {
+                dp[i] = true;
+            }
+        }
+        return dp[n - 1];
+    }
+}
