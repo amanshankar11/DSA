2cb0659e9145d18bf9ea7e3e55eae6563be5e2db
AMAN SHANKAR <153946946+amanshankar11@users.noreply.github.com>
2026-09-04 10:30:32 +0530

    Time: 1 ms (63.6%), Space: 46.7 MB (98.26%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -2,34 +2,33 @@ class Solution {
     public boolean isBipartite(int[][] graph) {
         int[] color=new int[graph.length];
         List<List<Integer>> g=new ArrayList<>();
-        for(int i=0;i<graph.length;i++) g.add(new ArrayList<>());
+        // for(int i=0;i<graph.length;i++) g.add(new ArrayList<>());
+        // for(int i=0;i<graph.length;i++){
+        //     for(int j=0;j<graph[i].length;j++){
+        //         g.get(i).add(graph[i][j]);
+        //     }
+        // }
         for(int i=0;i<graph.length;i++){
-            for(int j=0;j<graph[i].length;j++){
-                g.get(i).add(graph[i][j]);
-            }
-        }
-        for(int i=0;i<g.size();i++){
             if(color[i]==0){
-                if(!bfs(g,color,i)){
+                if(!bfs(graph,color,i)){
                     return false;
                 }
             }
         }
         return true;
     }
-    private boolean bfs(List<List<Integer>> graph,int[] color,int start){
+    private boolean bfs(int[][] graph,int[] color,int start){
         Queue<Integer> q=new LinkedList<>();
-        // visited[start]=true;
         color[start]=1;
         q.offer(start);
         while(!q.isEmpty()){
             int curr=q.poll();
-            for(int i=0;i<graph.get(curr).size();i++){
-                if(color[graph.get(curr).get(i)]==0){
-                    color[graph.get(curr).get(i)]=-color[curr];
-                    q.offer(graph.get(curr).get(i));
+            for(int i=0;i<graph[curr].length;i++){
+                if(color[graph[curr][i]]==0){
+                    color[graph[curr][i]]=-color[curr];
+                    q.offer(graph[curr][i]);
                 }
-                else if(color[curr]==color[graph.get(curr).get(i)]){
+                else if(color[curr]==color[graph[curr][i]]){
                     return false;
                 }
             }
