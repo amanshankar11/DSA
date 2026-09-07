eed0548b99be34085d3f5dbe3c5c4a708cb78802
AMAN SHANKAR <153946946+amanshankar11@users.noreply.github.com>
2026-09-07 10:50:43 +0530

    Time: 2 ms (88.7%), Space: 44.4 MB (24.63%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,72 +1,40 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-
-        Queue<int[]> q = new LinkedList<>();
-
-        int[][] directions = {
-            {1, 0},
-            {-1, 0},
-            {0, 1},
-            {0, -1}
-        };
-
-        int fresh = 0;
-
-        // Add all rotten oranges
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-
-                if (grid[i][j] == 2) {
-                    q.offer(new int[]{i, j});
+        Queue<int[]> q=new LinkedList<>();
+        int[][] directions={{-1,0},{1,0},{0,1},{0,-1}};
+        int fresh=0;
+        int minutes=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==2){
+                    q.offer(new int[]{i,j});
                 }
-                else if (grid[i][j] == 1) {
+                else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
+        while(!q.isEmpty() && fresh>0){
+            int size=q.size();
+            while(size-- > 0){
+                int[] curr=q.poll();
+                int i=curr[0];
+                int j=curr[1];
 
-        int minutes = 0;
+                for(int[] dir:directions){
+                    int ni=i+dir[0];
+                    int nj=j+dir[1];
 
-        while (!q.isEmpty() && fresh > 0) {
-
-            // All oranges currently in queue
-            // act during the same minute
-            int size = q.size();
-
-            while (size-- > 0) {
-
-                int[] curr = q.poll();
-
-                int i = curr[0];
-                int j = curr[1];
-
-                for (int[] dir : directions) {
-
-                    int ni = i + dir[0];
-                    int nj = j + dir[1];
-
-                    if (ni >= 0 && ni < grid.length &&
-                        nj >= 0 && nj < grid[0].length &&
-                        grid[ni][nj] == 1) {
-
-                        // Fresh -> Rotten
-                        grid[ni][nj] = 2;
-
+                    if(ni>=0 && ni<grid.length && nj>=0 && nj<grid[0].length && grid[ni][nj]==1){
                         fresh--;
-
-                        q.offer(new int[]{ni, nj});
+                        grid[ni][nj]=2;
+                        q.offer(new int[]{ni,nj});
                     }
                 }
             }
-
             minutes++;
         }
-
-        // Some fresh oranges could not be reached
-        if (fresh > 0) {
-            return -1;
-        }
-
+        if(fresh>0) return -1;
         return minutes;
     }
 }
