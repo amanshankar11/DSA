21b079da9777dc192ddcb97a3d37171a19eab2a7
AMAN SHANKAR <153946946+amanshankar11@users.noreply.github.com>
2026-09-14 21:41:04 +0530

    Time: 9 ms (17.19%), Space: 47.3 MB (21.2%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree=new int[numCourses];
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++) graph.add(new ArrayList<>());
+        for(int[] row:prerequisites){
+            int u=row[0];
+            int v=row[1];
+            indegree[v]++;
+            graph.get(u).add(v);
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0) q.offer(i);
+        }
+        int index=0;
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            index++;
+            for(int i=0;i<graph.get(curr).size();i++){
+                indegree[graph.get(curr).get(i)]--;
+                if(indegree[graph.get(curr).get(i)]==0){
+                    q.offer(graph.get(curr).get(i));
+                }
+            }
+        }
+        if(index!=numCourses) return false;
+        return true;
+    }
+}
