6e6c1f41f41ff3089098e52ddbe7b029f1140a1a
AMAN SHANKAR <153946946+amanshankar11@users.noreply.github.com>
2026-09-16 19:56:56 +0530

    Time: 24 ms (42.15%), Space: 63.4 MB (70.92%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,18 +1,13 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-
-        if(n == 1){
+        if(n==1){
             return new ArrayList<>(Arrays.asList(0));
         }
-
         List<List<Integer>> g = new ArrayList<>();
-
         for(int i=0;i<n;i++){
             g.add(new ArrayList<>());
         }
-
         int[] degree = new int[n];
-
         for(int[] edge:edges){
             int u = edge[0];
             int v = edge[1];
@@ -34,33 +29,23 @@ class Solution {
 
         int remaining = n;
 
-        while(remaining > 2){
-
+        while(remaining>2){
             int size = q.size();
-
             remaining -= size;
-
             for(int i=0;i<size;i++){
-
                 int leaf = q.poll();
-
-                for(int nei:g.get(leaf)){
-
-                    degree[nei]--;
-
-                    if(degree[nei] == 1){
-                        q.offer(nei);
+                for(int j=0;j<g.get(leaf).size();j++){
+                    degree[g.get(leaf).get(j)]--;
+                    if(degree[g.get(leaf).get(j)] == 1){
+                        q.offer(g.get(leaf).get(j));
                     }
                 }
             }
         }
-
         List<Integer> ans = new ArrayList<>();
-
         while(!q.isEmpty()){
             ans.add(q.poll());
         }
-
         return ans;
     }
 }
