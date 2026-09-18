514391a0ac30c7de51a8094819bbaf554cfa6f35
AMAN SHANKAR <153946946+amanshankar11@users.noreply.github.com>
2026-09-18 21:09:40 +0530

    Time: 6 ms (8.42%), Space: 47.2 MB (52.26%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,26 +1,29 @@
 class Solution {
-    private int helper(int[][] dp,String s1,String s2,int i,int j){
-        // if(i>=s1.length() || j>=s2.length()) return 0;
-        if(i==s1.length()){
-            return s2.length()-j;
+    private int helper(int i,int j,int[][] dp,String word1,String word2){
+        if(i>=word1.length()){
+            return word2.length()-j;
         }
-        if(j==s2.length()){
-            return s1.length()-i;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-            return helper(dp,s1,s2,i+1,j+1);
-        }
-        int delete=1+helper(dp,s1,s2,i+1,j);
-        int replace=1+helper(dp,s1,s2,i+1,j+1);
-        int insert=1+helper(dp,s1,s2,i,j+1);
 
-        return dp[i][j]=Math.min(delete,Math.min(replace,insert));
+        if(j>=word2.length()){
+            return word1.length()-i;
+        }
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(word1.charAt(i)==word2.charAt(j)){
+            return helper(i+1,j+1,dp,word1,word2);
+        }
+        int insert=1+helper(i,j+1,dp,word1,word2);
+        int delete=1+helper(i+1,j,dp,word1,word2);
+        int replace=1+helper(i+1,j+1,dp,word1,word2);
+
+        return dp[i][j]=Math.min(insert,Math.min(delete,replace));
     }
     public int minDistance(String word1, String word2) {
         int[][] dp=new int[word1.length()][word2.length()];
-        for(int[] row:dp) Arrays.fill(row,-1);
-        int min=helper(dp,word1,word2,0,0);
-        return min;
+        for(int[] r:dp){
+            Arrays.fill(r,-1);
+        }
+        return helper(0,0,dp,word1,word2);
     }
 }
